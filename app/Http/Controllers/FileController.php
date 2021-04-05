86c7b24e9bea54a637f15bac130962d0f6308f9b
mahfuz380 <almahfuz380@gmail.com>
2021-04-05 17:35:27 +0600

    text file to database upload done

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+        //$contents = fopen("DATA1.txt", 'r');
+
+        if (Storage::disk('local')->exists('DATA1.txt')) {
+        
+            $contents = Storage::get('DATA1.txt');
+            $arr = [];
+            $getData = explode(PHP_EOL, $contents);
+            // dd($get  Data);
+              foreach ($getData as $data1) {
+                 $arr[] = [
+                     'data'=>$data1
+                 ];
+              }
+            //   dd($arr);
+            //$file->save();
+            File::insert($arr);
+            return 'success';
+           
+        }else{
+            return "no data";
+        }
+        
+        //Storage::disk('local')->put('example.txt', 'Contents');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function show(File $file)
+    {
+        //
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(File $file)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, File $file)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(File $file)
+    {
+        //
+    }
+}
